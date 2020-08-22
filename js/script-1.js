76f7799428d7d2a0bfa23e5fab7dc7588dd61317
Alexander Khmyzenko <alexander193800@gmail.com>
2020-08-22 23:13:43 +0200

    Callback функция

--- js/script-1.js
+++ js/script-1.js
@@ -1,40 +1,35 @@
-// CRUD для свойств объекта
-// С - create(создать)
-// R - read(читать)
-// U - update(обновить)
-// D - delete (удалить)
-// Напиши скрипт, который, для объекта user, последовательно:
+// Callback функция
+// Функция mapArray(array, cb), принимает 1 - м параметром array - массив чисел,
+//   а вторым параметром cb - функцию обратного вызова(callback).
+// Функция mapArray создает новый массив numbers и заполняет его числами из массива array преобразованными функцией cb.
 
-// добавляет поле 'mood' со значением 'happy'
-// добавляет поле 'full time' со значением true
-// заменяет значение 'hobby' на 'skydiving'
-// заменяет значение 'premium' на false
-// в переменную message записывает содержимое объекта user:
-// для переменной keys присваивает массив свойств объекта, используя метод Object.keys()
-// с помощью оператора for...of
-// в формате ключ: значение
-// c переносом строки('\n')
+// Оформи создание массива numbers нужной длины используя new Array() и необходимый аргумент для задания длины,
+//   равной длине array.
 
-let message = "";
-const user = {
-  age: 20,
-  hobby: "html",
-  name: "Mango",
-  premium: true,
-};
-console.log(user);
+// Напиши функцию обратного вызова addIndex, которая принимает два параметра - element и index
+// и возвращает число - сумму element и index(сложение).
 
-user.hobby = "skydiving";
-user.premium = false;
-user.mood = "happy";
-user["full time"] = true;
+// Напиши функцию обратного вызова subIndex, которая принимает два параметра - element и index
+// и возвращает число - разность element и index(вычитание).
 
-console.log(user);
+const addIndex = (element, index) => element + index;
+const subIndex = (element, index) => element - index;
 
-const keys = Object.keys(user);
-console.log(keys);
-
-for (const key of keys) {
-  message += `${key}  :  ${user[key]}\n`;
+function mapArray(array, cb) {
+  "use strict";
+  const numbers = new Array(array.length);
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+    numbers[i] = cb(element, index);
+  }
+  return numbers;
 }
-console.log(message);
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log(mapArray(arr, addIndex));
+// [1, 3, 5, 7, 9]
+
+console.log(mapArray(arr, subIndex));
+// [1, 1, 1, 1, 1]
